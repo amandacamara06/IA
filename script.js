a26f3a26aa826cc0244f7c87b05a29e455107d76
amandacamara06 <129406171+amandacamara06@users.noreply.github.com>
2024-05-23 09:44:33 -0300

    23/05

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alernativas: ["Alternativa 1", "Alternativa 2"],
+    },
+
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+]
